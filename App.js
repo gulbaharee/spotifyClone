90c88a969dcf8679993daa59b0992f2337dec177
Gülbahar Erol <ggulbaharerol@gmail.com>
2022-09-17 11:20:37 +0300

    first init

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App  = () => {
+ 
+  return (
+   <View>
+
+   </View>
+  );
+};
+
+export default App;
